bf2be401a9166777fb2ed9caf2ff9364faa2b52d
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-05-31 11:00:50 +0100

    + or -

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number:d} is postive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative")
+    
