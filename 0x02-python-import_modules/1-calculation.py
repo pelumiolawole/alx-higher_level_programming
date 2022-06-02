484ccef001670a9dcb1d69a12e616f6c4af36f65
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-02 22:55:50 +0100

    Py Import Modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    
+def add(a, b):
+    a = 10
+    b = 5
+    return (a + b)
+add()
+
+def sub(a, b):
+    a = 10
+    b = 5
+    return (a - b)
+sub()
+
+def mul(a, b):
+    a = 10
+    b = 5
+    return (a * b)
+mul()
+
+def div(a, b):
+    a = 10
+    b = 5
+    return (a / b)
+div()
