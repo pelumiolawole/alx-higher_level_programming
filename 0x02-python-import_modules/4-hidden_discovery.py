d248b5226741268c24abab202f1538ff480761bd
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-03 00:47:42 +0100

    Py Import Modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
         if i[:2] != "__":
-            print("{:i}".format(s))
-            
-            
+            print("{:i}".format(i))
+ 
