9b62470eac2bef045d1808027d81ac7075497e89
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-03 01:01:15 +0100

    Py Import Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,11 @@
 from sys import argv
 
 if __name__ == "__main__":
-
-    def no_of_argu(*args):
-    # using len() method in args to count
-        return (len(args))
     i = 1
     args = len(argv) - 1
 
-    print(f"{args:d} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
     print(f"{'.' if args == 0 else ':'}")
     while (i <= args):
         print(f"{i}: {argv[i]}")
         i += 1
-
-no_of_argu(argv)
