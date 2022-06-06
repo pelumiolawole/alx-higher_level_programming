a6aa1d9f28d67e5a03d24a626443f58e11a74046
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-06 22:26:07 +0100

    Data

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from random import random
-mylist = []
-mylist.append(random)
 def print_list_integer(my_list=[]):
-    print_list_integer = __import__('0-print_list_integer').print_list_integer
-    print(mylist) 
-    
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
