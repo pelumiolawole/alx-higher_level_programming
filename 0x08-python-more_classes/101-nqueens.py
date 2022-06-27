771fbc3375c8db0e5734e13f9e8126fb1af26f67
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-06-27 21:49:20 +0100

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,87 +1,66 @@
-"""Find all possible chess-board combinations of size n*n with n queens,
-where queens are represented by the digit 2"""
-from collections import Counter
-import numpy as np
+# Function to check if two queens threaten each other or not
+def isSafe(mat, r, c):
+
+	# return false if two queens share the same column
+	for i in range(r):
+		if mat[i][c] == 'Q':
+			return False
+
+	# return false if two queens share the same `\` diagonal
+	(i, j) = (r, c)
+	while i >= 0 and j >= 0:
+		if mat[i][j] == 'Q':
+			return False
+		i = i - 1
+		j = j - 1
+
+	# return false if two queens share the same `/` diagonal
+	(i, j) = (r, c)
+	while i >= 0 and j < len(mat):
+		if mat[i][j] == 'Q':
+			return False
+		i = i - 1
+		j = j + 1
+
+	return True
 
 
-def block_positions(chess_board, row, column, limit):
-    """Fills all squares that can no longer contain a queen with the value -1
-    There are a maximum of 8 directions that must be blocked from a given square"""
-    # Block left
-    for col in range(column - 1, -1, -1):
-        chess_board[row, col] = 0
-    # Block right
-    for col in range(column + 1, limit):
-        chess_board[row, col] = 0
-    # Block up
-    for rw in range(row - 1, -1, -1):
-        chess_board[rw, column] = 0
-    # Block down
-    for rw in range(row + 1, limit):
-        chess_board[rw, column] = 0
-    # Block L up-diag
-    rw = row
-    col = column
-    while rw > 0 and col > 0:
-        rw -= 1
-        col -= 1
-        chess_board[rw, col] = 0
-    # Block L down-diag
-    rw = row
-    col = column
-    while rw < limit - 1 and col > 0:
-        rw += 1
-        col -= 1
-        chess_board[rw, col] = 0
-    # Block R up-diag
-    rw = row
-    col = column
-    while rw > 0 and col < limit - 1:
-        rw -= 1
-        col += 1
-        chess_board[rw, col] = 0
-    # Block R down-diag
-    rw = row
-    col = column
-    while rw < limit - 1 and col < limit - 1:
-        rw += 1
-        col += 1
-        chess_board[rw, col] = 0
-    return chess_board
+def printSolution(mat):
+	for r in mat:
+		print(str(r).replace(',', '').replace('\'', ''))
+	print()
 
 
-def initialise_board(num):
-    """Build the empty board"""
-    board = np.ones(num * num).reshape(num, num)
-    return board
+def nQueen(mat, r):
+
+	# if `N` queens are placed successfully, print the solution
+	if r == len(mat):
+		printSolution(mat)
+		return
+
+	# place queen at every square in the current row `r`
+	# and recur for each valid movement
+	for i in range(len(mat)):
+
+		# if no two queens threaten each other
+		if isSafe(mat, r, i):
+			# place queen on the current square
+			mat[r][i] = 'Q'
+
+			# recur for the next row
+			nQueen(mat, r + 1)
+
+			# backtrack and remove the queen from the current square
+			mat[r][i] = '–'
 
 
-def valid_boards(board, row, num):
-    """Find all valid N-queen boards"""
-    global counter
-    while row < num:
-        indices = [index for index in range(num) if board[row, index] == 1]
-        if indices == []:
-            return False
-        for index in indices:
-            old_board = board.copy()
-            board[row, index] = 2
-            board = block_positions(board, row, index, num)
-            is_possible = valid_boards(board, row + 1, num)
-            board = old_board
-            if not is_possible and index == indices[-1]:
-                return False
-    flattened = Counter(board.flatten())
-    if flattened[2] == num:
-        print(board)
-        print()
-        counter += 1
+if __name__ == '__main__':
 
+	# `N × N` chessboard
+	N = 8
 
-if __name__ == "__main__":
-    counter = 0
-    num = 5
-    board = initialise_board(num)
-    valid_boards(board, row=0, num=num)
-    print(counter, "solutions")
-    print("Finished")
+	# `mat[][]` keeps track of the position of queens in
+	# the current configuration
+	mat = [['–' for x in range(N)] for y in range(N)]
+
+	nQueen(mat, 0)
