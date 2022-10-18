6f0e0fb46bcde0f42874cb97d8aab8c885200130
Pelumi Olawole <olawolepelumisunday@gmail.com>
2022-10-19 00:19:31 +0100

    0-readme updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
-const request = require('request');
-
-const requestURL = process.argv[2];
-const users = {};
-
-request(requestURL, function (err, response, body) {
-  if (err) {
-    return console.log(err);
+/* A script that reads and prints the content of a file. */
+const fs = require("fs");
+const arg = process.argv[2];
+fs.readFile(arg, "utf-8", function (error, content) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
   }
-
-  const pBody = JSON.parse(body);
-
-  for (let i = 0; i < pBody.length; i++) {
-    const tmp = pBody[i].userId;
-    if (pBody[i].completed) {
-      users[tmp] = (users[tmp] || 0) + 1;
-    }
-  }
-
-  console.log(users);
 });
